341d5b130e43b76f6c35af1f6a1a41882d629153
kimuch <lxanthonykim@gmail.com>
2025-11-12 13:05:36 +0300

    task 2 of messaging app

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Conversation, Message
+from .serializers import ConversationSerializer,MessageSerializer
 # Create your views here.
+
+class Conversation(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = ConversationSerializer
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer = MessageSerializer
